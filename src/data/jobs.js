e40f75e95d9c867b848428b6fbf06d923e98ac7d
Jay Posten <jdposten@gmail.com>
2021-12-14 16:53:43 -0600

    Added new job

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -16,36 +16,43 @@ module.exports = [
 	{
 		title: 'Web Developer',
 		company: 'USDM.net',
-		description: 'Experience with large-scale websites and media/email campaign development. HTML, CSS, responsive website/email development, jQuery.',
+		description: 'Building and maintaining large-scale websites and media/email campaign development. HTML, CSS, responsive website/email development, jQuery.',
 		date: 'May 2008–January 2015',
 		img: 'job_usdm'
 	},
 	{
 		title: 'Web Developer',
 		company: 'Orange142',
-		description: 'Website and media/email development. HTML, CSS, responsive website/email development.',
+		description: 'Website, landing page, and email development. HTML, CSS, responsive website/email development.',
 		date: 'January 2015–July 2016',
 		img: 'job_orange142'
 	},
 	{
 		title: 'Web Developer',
 		company: 'USDM.net',
-		description: 'Experience with large-scale websites and media/email campaign development. HTML, CSS, responsive website/email development, jQuery. Exposure to Angular and React.',
+		description: 'Building and maintaining large-scale websites and media/email campaign development. HTML, CSS, responsive website/email development, jQuery. Exposure to Angular and React.',
 		date: 'July 2016–July 2018',
 		img: 'job_usdm_2'
 	},
 	{
 		title: 'Software Engineer II',
 		company: 'Saatva',
-		description: 'Full stack work on large e-commerce platforms. HTML, SASS, JavaScript, React, Node.js, Express.js, PHP, GraphQL, SQL.',
+		description: 'Full-stack work on a large e-commerce platform. HTML, SASS, JavaScript, React, Node.js, Express.js, PHP, GraphQL, SQL.',
 		date: 'November 2018–December 2020',
 		img: 'job_saatva'
 	},
 	{
 		title: 'Tech Lead / Senior Software Engineer',
 		company: 'Saatva',
-		description: 'Tech lead + full stack work on large e-commerce platforms. HTML, SASS, JavaScript, React, Node.js, Express.js, Next.js, GraphQL.',
-		date: 'December 2020–Present',
+		description: 'Tech lead + full-stack work on a large e-commerce platform. HTML, SASS, JavaScript, React, Node.js, Express.js, Next.js, GraphQL.',
+		date: 'December 2020–December 2021',
 		img: 'job_saatva_2'
+	},
+	{
+		title: 'Senior Software Engineer',
+		company: 'WorkBoard',
+		description: 'Full-stack work on a SaaS platform focused on helping companies manage OKRs. HTML, SASS, JavaScript, React, Node.js',
+		date: 'January 2022–Present',
+		img: 'job_workboard'
 	}
 ]
